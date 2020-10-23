e16fc0c6bf23b9dfa2170bd144762d95cf265cdd
Landon Labrum <landonlabrum@Landons-MBP.hsd1.ut.comcast.net>
2020-10-23 16:45:07 -0600

    Updates

---
+++ precache-manifest.ad96537d3d3c14ee6f25644c7214b0d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0196e8c69944a8c464bedb483f489326",
+    "url": "/guardian1/index.html"
+  },
+  {
+    "revision": "8a48d42abee14042fc2d",
+    "url": "/guardian1/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "5c07cb31dbdb5a3dcf6d",
+    "url": "/guardian1/static/css/main.99410c1f.chunk.css"
+  },
+  {
+    "revision": "8a48d42abee14042fc2d",
+    "url": "/guardian1/static/js/2.9bcf66ba.chunk.js"
+  },
+  {
+    "revision": "f6721cc19e6bc4812e12becc59aae507",
+    "url": "/guardian1/static/js/2.9bcf66ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c07cb31dbdb5a3dcf6d",
+    "url": "/guardian1/static/js/main.2f5e6a2c.chunk.js"
+  },
+  {
+    "revision": "cf40173f6aa6bf10ed3b",
+    "url": "/guardian1/static/js/runtime-main.c19e28f5.js"
+  },
+  {
+    "revision": "74df33ee81d76dda39a14a5431dac0a1",
+    "url": "/guardian1/static/media/banner_1.74df33ee.png"
+  },
+  {
+    "revision": "6457a250ebe616ed8dc7c31b26a3f691",
+    "url": "/guardian1/static/media/banner_2.6457a250.png"
+  },
+  {
+    "revision": "33dd2caca095fe14fde47e6e515cabcd",
+    "url": "/guardian1/static/media/banner_3.33dd2cac.png"
+  },
+  {
+    "revision": "6527b1b8899d45f0161b9ed5f7c7f00f",
+    "url": "/guardian1/static/media/logo.6527b1b8.svg"
+  }
+]);
